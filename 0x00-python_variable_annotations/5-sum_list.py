c8f6846a4d663c6707cc526314ea16193e307ae1
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-11 15:55:04 +0300

    Use typing module List

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Define input_list function"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Get sum of list of floats"""
     return (float(sum(input_list)))
