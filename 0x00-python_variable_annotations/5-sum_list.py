70eb0995495bc60b178e9deb1aa625bc13f9f285
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-11 15:51:25 +0300

    list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Define input_list function"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Get sum of list of floats"""
+    return (float(sum(input_list)))
