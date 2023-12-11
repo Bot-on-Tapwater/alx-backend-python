f59352e34136802d67dc5aca095115dfa641f699
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-11 16:33:02 +0300

    Add tuple and list to imports

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,7 +3,7 @@
 Use mypy to validate the following piece of
 code and apply any necessary changes.
 """
-from typing import TypeVar, Any, Union, Mapping
+from typing import TypeVar, Any, Union, Mapping, Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
