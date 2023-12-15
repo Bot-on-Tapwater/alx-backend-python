562b6df2d1c0d424b8ed1dfa6b6b8a1c4151e788
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-15 11:51:30 +0300

    Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Implement async_comprehension"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """return 10 random numbers"""
+    return [i async for i in async_generator()]
