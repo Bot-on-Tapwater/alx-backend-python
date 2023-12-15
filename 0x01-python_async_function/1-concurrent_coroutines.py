8d5be94889a1655e988904b414476f58773f4076
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-15 10:35:17 +0300

    Sort the array using sorted

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-"""
-write an async routine, that will spawn the wait_random func n times
-"""
-
+"""Implement wait_n"""
+import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    write an async routine, that will spawn
-    the wait_random func n times
-    """
-    lst: list = []
-    for i in range(n):
-        res = await wait_random(max_delay)
-        lst.append(res)
-    return lst
+    """return list of delays"""
+    delays = [await wait_random(max_delay) for i in range(0, n)]
+
+    return sorted(delays)
