f91001780fdfcdda164b340ed5d20227ff37e82d
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-15 12:33:46 +0300

    Parallel comprehensions

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""Implement measure_runtime"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure total runtime"""
+    startTime = time.time()
+
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+
+    endTime = time.time()
+
+    elapsedTime = endTime - startTime
+
+    return elapsedTime
