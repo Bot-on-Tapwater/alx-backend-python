fb1bff36240ca662f0f0ebe683c688a1e63ea59e
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-15 11:12:14 +0300

    Tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Implement task_wait_random"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return asyncio.Task"""
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
