daf148e7c3f3fcef3ee81e38588f70c8f2ced889
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-15 11:34:08 +0300

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Implement async_generator"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator:
+    """yield rand no 0 - 10"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield (random.uniform(0, 10))
