65239bb936e99b0600d8d39fb87358e72612e391
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-15 10:20:33 +0300

    multiple coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Implement wait_n"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """return list of delays"""
+    delays = [await wait_random(max_delay) for i in range(0, n)]
+
+    return delays
