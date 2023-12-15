8902b2bc8db4e7c73f64705bb013fe0b602845c4
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-15 11:38:15 +0300

    Change return type annotation to Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """yield rand no 0 - 10"""
     for i in range(10):
         await asyncio.sleep(1)
